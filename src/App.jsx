4e01bbc17052a2cad8bfa00d1f51ec90009336c5
poylib <poy_lib@naver.com>
2022-12-20 17:17:19 +0900

    feature: init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
+import GlobalStyle from './GlobalStyle';
+import Home from './pages/Home';
+const App = () => {
+  return (
+    <>
+      <GlobalStyle />
+      <Routes>
+        <Route path='/' element={<Home />} />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
---
+++ src/GlobalStyle.jsx
@@ -0,0 +1,23 @@
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+  * {
+    box-sizing: border-box;
+    -webkit-tap-highlight-color: rgba(0,0,0,0);
+    -webkit-tap-highlight-color: transparent;
+    
+  }
+  
+  a {
+    text-decoration: none;
+  }
+
+  body {
+    font-family: 'Noto Sans KR', sans-serif;
+    max-width:100vw;
+    margin:0 auto;
+  }
+`;
+export default GlobalStyle;
