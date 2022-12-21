67035be3ad1ca7f48b8d30d70f415b1a41b02031
youbinpark <you8inpark@gmail.com>
2022-12-22 02:56:19 +0900

    style: add input font

--- src/components/WishModal/index.jsx
+++ src/components/WishModal/index.jsx
@@ -23,7 +23,7 @@ const WishModal = ({ setIsWishModal }) => {
             <div className='contents'>
               <input maxLength={8} placeholder='이름 또는 닉네임 (8글자 이하)' onChange={handleName} value={name} />
               <textarea maxLength={200} placeholder='소원을 작성해주세요!' onChange={handleWish} value={wish} />
-              <span>{wish.length}/200</span>
+              <span>{wish.length} / 200</span>
               <div className='image-wrapper'>
                 <img src='https://s3.us-west-2.amazonaws.com/secure.notion-static.com/fbdc7f7f-5e7d-483b-8c84-27623c81c508/%E1%84%83%E1%85%A1%E1%86%AF_%28%E1%84%8B%E1%85%A7%E1%84%87%E1%85%A2%E1%86%A8_%E1%84%8C%E1%85%A5%E1%86%A8%E1%84%80%E1%85%A6%29.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20221221%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20221221T091023Z&X-Amz-Expires=86400&X-Amz-Signature=83c8848f6281839b95ac2dc70233135a58078ae078b6977396d944f5f749e9f5&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22%25E1%2584%2583%25E1%2585%25A1%25E1%2586%25AF%2520%28%25E1%2584%258B%25E1%2585%25A7%25E1%2584%2587%25E1%2585%25A2%25E1%2586%25A8%2520%25E1%2584%258C%25E1%2585%25A5%25E1%2586%25A8%25E1%2584%2580%25E1%2585%25A6%29.png%22&x-id=GetObject' />
               </div>
@@ -91,8 +91,11 @@ const WishModalContainer = styled.div`
         border: none;
         background: transparent;
         color: ${({ theme }) => theme.mainColor};
-        font-size: 1.75rem;
+        font-family: 'UhBeeSeulvely';
+        font-size: 1.5rem;
         outline: none;
+
+        ${({ theme }) => theme.textFont1};
       }
 
       textarea {
@@ -113,7 +116,7 @@ const WishModalContainer = styled.div`
       span {
         display: block;
         color: #787878;
-        transform: translate(85%, -220%);
+        transform: translate(82%, -220%);
       }
 
       .image-wrapper {
