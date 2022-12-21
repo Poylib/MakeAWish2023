4dacd6014399d6239817615297aa67be01f503e9
poylib <poy_lib@naver.com>
2022-12-21 15:52:39 +0900

    feature: develop init

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+const Home = () => {
+  return (
+    <StyledHome>
+      <p>Home</p>
+    </StyledHome>
+  );
+};
+export default Home;
+
+const StyledHome = styled.div``;
---
+++ src/pages/HomeLoading.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+const HomeLoading = () => {
+  return (
+    <StyledHomeLoading>
+      <p>Loading</p>
+    </StyledHomeLoading>
+  );
+};
+
+export default HomeLoading;
+
+const StyledHomeLoading = styled.div``;
