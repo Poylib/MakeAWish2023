99285d051c11f26b0295f985e362eed8f7f8cf81
jeong-yewon <jdc209802@gmail.com>
2022-12-22 01:11:53 +0900

    test 모달 테스트 버튼 추가

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,6 +4,7 @@ import WishModal from '../../components/WishModal';
 import Button from '../../components/Button';
 import { headercolor, blackcolor } from '../../theme';
 import MainBackground from '../../components/MainBackground';
+import { Link } from 'react-router-dom';
 const Home = () => {
   const [isWishModal, setIsWishModal] = useState(false);
 
@@ -21,6 +22,7 @@ const Home = () => {
           }}
           text='소원빌기'
         />
+        <Link to='/main'>모달 테스트 링크 버튼</Link>
       </article>
       <MainBackground />
       {isWishModal && <WishModal isWishModal={isWishModal} setIsWishModal={setIsWishModal} />}
