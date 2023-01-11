62605f072e1a9f0804779b2f17e56e5aabcc663d
poylib <poy_lib@naver.com>
2023-01-11 13:41:56 +0900

    style: 복주머니 랜덤 배치

---
+++ src/constant/bok/index.jsx
@@ -0,0 +1,11 @@
+import bok1 from '../../assets/main/pockets/004.png';
+import bok2 from '../../assets/main/pockets/005.png';
+import bok3 from '../../assets/main/pockets/006.png';
+import bok4 from '../../assets/main/pockets/007.png';
+import bok5 from '../../assets/main/pockets/008.png';
+import bok6 from '../../assets/main/pockets/009.png';
+import bok7 from '../../assets/main/pockets/010.png';
+import bok8 from '../../assets/main/pockets/011.png';
+import bok9 from '../../assets/main/pockets/012.png';
+
+export const imgArr = [bok1, bok2, bok3, bok4, bok5, bok6, bok7, bok8, bok9];
