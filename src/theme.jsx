686dc4cae2d754a03a366b2589c3936e857cba37
jeong-yewon <jdc209802@gmail.com>
2023-01-17 02:32:28 +0900

    style: style 수정

--- src/theme.jsx
+++ src/theme.jsx
@@ -4,7 +4,8 @@ export const blackcolor = '#2D2D2D';
 
 export const mainColor = '#EA7048';
 export const contentFontColor = '#2D2D2D';
-export const bgColor = 'E4DDCC';
+export const bgColor = '#E4DDCC';
+export const boxBgColor = '#F6EFE5';
 export const wishButton = '#FD8A69';
 export const redButton = '#CC3333';
 
