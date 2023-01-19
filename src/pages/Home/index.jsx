8028a3122a38cb8d6678f3043eebbeef3d1c93eb
youbinpark <you8inpark@gmail.com>
2023-01-20 01:46:52 +0900

    fix: conflict

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { api } from '../../api';
 import styled from 'styled-components';
-import { useNavigate } from 'react-router-dom';
 import { BsArrowCounterclockwise } from 'react-icons/bs';
 import useStore from '../../context/store';
 import MakeWishModal from '../../components/MakeWishModal';
@@ -46,7 +45,7 @@ const Home = () => {
     getWishCheck();
     getWishCounts();
     if (wroteWish.length <= 7) getWish();
-  }, [isMakeWish]);
+  }, [isMakeWishModal]);
 
   const getWish = async () => {
     try {
