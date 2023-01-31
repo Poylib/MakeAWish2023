77bb70d7b368d190ed83ac7218788051ffba71aa
poylib <poy_lib@naver.com>
2023-01-31 16:27:48 +0900

    feature: ts setting

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command, mode }) => ({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
   server: {
     port: 3000,
